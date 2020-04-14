6209aed9e5856a1f72c210bf497d05e873375cfc
Дмитрий Брезгунов <dmitry.brezgunov@yandex.ru>
2020-04-15 01:51:52 +0300

    add weather by city

--- app.py
+++ app.py
@@ -15,24 +15,33 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     username = message.from_user.username
-    greeting = f'Привет, {username}! \n \n'
+    greeting = f'Привет, {username}! Введи название города и я расскажу тебе о погоде в нем.'
     bot.send_message(message.from_user.id, greeting)
 
-def get_weather():
-    params = {"appid": OPENWEATHER_TOKEN, "q": "Moscow", "lang": "ru", "units": "metric"}
+def get_weather(place):
+    params = {"appid": OPENWEATHER_TOKEN, "q": place, "lang": "ru", "units": "metric"}
     api_result = requests.get('http://api.openweathermap.org/data/2.5/weather', params)
     api_response = api_result.json()
-    return f"Сейчас в Москве {api_response['main']['temp']} градусов"
+    if api_response['code'] == 404:
+        return f'Извини, я не нашел такого города. Проверь, правильно ли введено название.'
+    condition = api_response['weather']['description']
+    temp = round(api_response['main']['temp'])
+    feel_temp = round(api_response['main']['feels_like'])
+    pressure = api_response['main']['pressure']//133.3224*100
+    humidity = api_response['main']['humidity']
+    wind = api_response['wind']['speed']
+    return f"Сейчас в {place} {temp}°С, ощущается как {feel_temp}°С, {condition}.\n"\
+           f"Давление {pressure} мм. ртутного столба. Влажность воздуха {humidity}.\n"\
+           f"Скорость ветра {wind} м/с."
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    bot.send_message(message.from_user.id, f"Привет, сейчас я расскажу тебе погоду в Москве")
-    weather = get_weather()
+    weather = get_weather(message.text)
     bot.send_message(message.from_user.id, weather)
 
 while True:
     try:
         bot.polling(none_stop=True, interval=0)
     except ConnectionError as e:
-        print(f">>>>>> error: {e}")
+        print(f"Бот упал с ошибкой: {e}")
         time.sleep(5)
