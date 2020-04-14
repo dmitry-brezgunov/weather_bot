d117a0c7c4ae05d00509cf6b7654f7f746b14043
Дмитрий Брезгунов <dmitry.brezgunov@yandex.ru>
2020-04-14 22:55:05 +0300

    adding weather

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import time
 import telebot
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,6 +17,17 @@ def start(message):
     username = message.from_user.username
     greeting = f'Привет, {username}! \n \n'
     bot.send_message(message.from_user.id, greeting)
+
+def get_weather():
+    params = {"appid": "OPENWEATHER_TOKEN", "q": "Moscow", "lang": "ru", "units": "metric"}
+    api_result = requests.get('https://api.openweathermap.org/data/2.5/weather', params)
+    api_response = api_result.json()
+    return f"Сейчас в Москве {api_response['main']['temp']} градусов"
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    bot.send_message(message.from_user.id, f"Привет, сейчас я расскажу тебе погоду в Москве")
+    get_weather()
 
 while True:
     try:
