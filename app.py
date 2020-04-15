de327db24d2aaf2f765e9e53314cdb860ba23b6b
Дмитрий Брезгунов <dmitry.brezgunov@yandex.ru>
2020-04-15 15:36:26 +0300

    some refactoring

--- app.py
+++ app.py
@@ -19,12 +19,10 @@ def start(message):
     bot.send_message(message.from_user.id, greeting)
 
 def weather_forcast(api_response):
-    if api_response['cod'] == "404":
-        return f'Извини, я не нашел такого города. Проверь, правильно ли введено название.'
     condition = api_response['weather'][0]['description']
     temp = round(api_response['main']['temp'])
     feel_temp = round(api_response['main']['feels_like'])
-    pressure = api_response['main']['pressure']//133.3224*100
+    pressure = round(api_response['main']['pressure']*100//133.3224)
     humidity = api_response['main']['humidity']
     wind = api_response['wind']['speed']
     return f"Сейчас {temp}°С, ощущается как {feel_temp}°С, {condition}.\n"\
@@ -33,29 +31,30 @@ def weather_forcast(api_response):
 
 def weather_recomendations(api_response):
     if api_response['weather'][0]['id'] in [731, 751, 761, 762, 771, 781]:
-        return "На улице ужасная погода, сиди дома."
-    elif api_response['main']['temp'] < -20:
-        return "На улице очень холодно. Может лучше остаться дома?"
+        recomendation = "На улице ужасная погода, сиди дома."
+    elif api_response['main']['feels_like'] < -20:
+        recomendation = "На улице очень холодно. Может лучше остаться дома?"
     elif api_response['weather'][0]['id'] in [602, 613, 621, 622]:
-        return "На улице метель. Может лучше остаться дома?."
-    elif api_response['main']['temp'] < 0:
-        return "На улице холодно, одевайся теплее."
+        recomendation = "На улице метель. Может лучше остаться дома?."
+    elif api_response['main']['feels_like'] < 0:
+        recomendation = "На улице холодно, одевайся теплее."
     elif api_response['weather'][0]['id'] in range(200, 233):
-        return "На улице гроза. Может лучше остаться дома?"
+        recomendation = "На улице гроза. Может лучше остаться дома?"
     elif api_response['weather'][0]['id'] in [502, 503, 504, 522, 531]:
-        return "На улице ливень. Может лучше остаться дома?"
+        recomendation = "На улице ливень. Может лучше остаться дома?"
     elif api_response['weather'][0]['id'] in [500, 501, 520, 521]:
-        return "На улице дождь, не забудь взять зонт."
-    elif api_response['main']['temp'] < 15:
-        return "На улице прохладно, лучше надень куртку."
-    elif api_response['main']['temp'] < 25 and api_response['weather'][0]['id'] in range(800, 805):
-        return "На улице хорошая погода, самое время для прогулки."
-    elif api_response['main']['temp'] < 35 and api_response['weather'][0]['id'] == 800:
-        return "На улице жарко и солнечно, старайся держаться тени."
-    elif api_response['main']['temp'] < 35:
-        return "На улице жарко, можно надеть шорты."
+        recomendation = "На улице дождь, не забудь взять зонт."
+    elif api_response['main']['feels_like'] < 15:
+        recomendation = "На улице прохладно, лучше надень куртку."
+    elif api_response['main']['feels_like'] < 25 and api_response['weather'][0]['id'] in range(800, 805):
+        recomendation = "На улице хорошая погода, самое время для прогулки."
+    elif api_response['main']['feels_like'] < 35 and api_response['weather'][0]['id'] == 800:
+        recomendation = "На улице жарко и солнечно, старайся держаться тени."
+    elif api_response['main']['feels_like'] < 35:
+        recomendation = "На улице жарко, можно надеть шорты."
     else:
-        return "На улице очень жарко, старайся пить больше воды."
+        recomendation = "На улице очень жарко, старайся пить больше воды."
+    return recomendation
 
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
@@ -63,8 +62,13 @@ def get_weather(message):
     params = {"appid": OPENWEATHER_TOKEN, "q": city, "lang": "ru", "units": "metric"}
     api_result = requests.get('http://api.openweathermap.org/data/2.5/weather', params)
     api_response = api_result.json()
-    bot.send_message(message.from_user.id, weather_forcast(api_response))
-    bot.send_message(message.from_user.id, weather_recomendations(api_response))
+    if api_response['cod'] == "404":
+        bot.send_message(message.from_user.id,
+                         'Извини, я не нашел такого города. Проверь, правильно ли введено название.')
+    forcast = weather_forcast(api_response)
+    recomendation = weather_recomendations(api_response)
+    bot.send_message(message.from_user.id, forcast)
+    bot.send_message(message.from_user.id, recomendation)
 
 while True:
     try:
